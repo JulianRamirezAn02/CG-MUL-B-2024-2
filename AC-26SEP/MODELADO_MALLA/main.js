46384d3be6a60ee6b2d8c6e2be815b1c7f40189d
JulianRamirez02 <julipipe0207@gmail.com>
2024-10-01 23:06:05 -0500

    Cambio de color, visible lineas de vertices y funcion aleatoria

--- AC-26SEP/MODELADO_MALLA/main.js
+++ AC-26SEP/MODELADO_MALLA/main.js
@@ -1,22 +1,33 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+const geometryCube = new THREE.BoxGeometry(10, 10, 10);
+const materialCube = new THREE.MeshBasicMaterial({ color: 0xff0000 }); 
+const cube = new THREE.Mesh(geometryCube, materialCube);
+cube.position.x = -20; 
+scene.add(cube);
+
+
+const edgesCube = new THREE.EdgesGeometry(geometryCube);
+const lineCube = new THREE.LineSegments(edgesCube, new THREE.LineBasicMaterial({ color: 0x000000 })); // Color negro
+cube.add(lineCube);
+
+
+camera.position.z = 50;
 
 function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate)
+    cube.rotation.x += Math.random() * 0.02; 
+    cube.rotation.y += Math.random() * 0.02; 
+    cube.position.x += (Math.random() - 0.5) * 0.5; 
+    renderer.render(scene, camera);
 }
+
+animate();
+
+
